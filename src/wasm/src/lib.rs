792213b279a02ebf9a67008553e463f077526ad6
hazae41 <4405263+hazae41@users.noreply.github.com>
2024-08-22 12:53:58 +0700

    mods

--- src/wasm/src/lib.rs
+++ src/wasm/src/lib.rs
@@ -1,30 +1,5 @@
 #![no_std]
 
-extern crate alloc;
+mod mods;
 
-use alloc::vec::Vec;
-use wasm_bindgen::prelude::*;
-
-#[wasm_bindgen]
-pub struct Memory {
-    #[wasm_bindgen(skip)]
-    pub inner: Vec<u8>,
-}
-
-#[wasm_bindgen]
-impl Memory {
-    #[wasm_bindgen(constructor)]
-    pub fn new(inner: Vec<u8>) -> Memory {
-        Memory { inner }
-    }
-
-    #[wasm_bindgen]
-    pub fn ptr(&self) -> *const u8 {
-        self.inner.as_ptr()
-    }
-
-    #[wasm_bindgen]
-    pub fn len(&self) -> usize {
-        self.inner.len()
-    }
-}
+pub use mods::*;
---
+++ src/wasm/src/mods/memory/mod.rs
@@ -0,0 +1,28 @@
+extern crate alloc;
+
+use alloc::vec::Vec;
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+pub struct Memory {
+    #[wasm_bindgen(skip)]
+    pub inner: Vec<u8>,
+}
+
+#[wasm_bindgen]
+impl Memory {
+    #[wasm_bindgen(constructor)]
+    pub fn new(inner: Vec<u8>) -> Memory {
+        Memory { inner }
+    }
+
+    #[wasm_bindgen]
+    pub fn ptr(&self) -> *const u8 {
+        self.inner.as_ptr()
+    }
+
+    #[wasm_bindgen]
+    pub fn len(&self) -> usize {
+        self.inner.len()
+    }
+}
