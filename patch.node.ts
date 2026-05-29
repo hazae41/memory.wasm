6c68162426786b5003a20ef48efa0412ea1407f7
hazae41 <4405263+hazae41@users.noreply.github.com>
2026-05-29 22:32:19 +0700

    f

--- patch.node.ts
+++ patch.node.ts
@@ -2,10 +2,8 @@ import fs from "node:fs"
 
 const { name } = JSON.parse(fs.readFileSync("./package.json", "utf8"))
 
-const slashes = "/..".repeat(name.split("/").length)
+const patched = fs.readFileSync("./out/wasm/Cargo.toml", "utf8")
+  .replaceAll("daemon", name.split("/")[1].replaceAll("-", "_"))
+  .replaceAll("../../node_modules", `../..${"/..".repeat(name.split("/").length)}`)
 
-const original = fs.readFileSync("./out/wasm/Cargo.toml", "utf8")
-
-const replaced = original.replaceAll("../../node_modules", `../..${slashes}`)
-
-fs.writeFileSync("./out/wasm/Cargo.toml", replaced)
+fs.writeFileSync("./out/wasm/Cargo.toml", patched)
