c711baa29f7a009025eeda175b58f2f8fde1e88c
hazae41 <4405263+hazae41@users.noreply.github.com>
2026-05-30 22:34:50 +0700

    f

--- run/prepack.ts
+++ run/prepack.ts
@@ -1,9 +1,14 @@
-import { readFileSync, writeFileSync } from "node:fs"
+import { cpSync, readFileSync, rmSync, writeFileSync } from "node:fs"
 
 const { name } = JSON.parse(readFileSync("./package.json", "utf8"))
+
+cpSync("./src/wasm", "./out/wasm", { recursive: true })
 
 const patched = readFileSync("./out/wasm/Cargo.toml", "utf8")
   .replaceAll("daemon", name.split("/")[1].replaceAll("-", "_"))
   .replaceAll("../../node_modules", `../..${"/..".repeat(name.split("/").length)}`)
 
 writeFileSync("./out/wasm/Cargo.toml", patched)
+
+rmSync("./out/wasm/target", { recursive: true, force: true })
+rmSync("./out/wasm/.gitignore", { force: true })
